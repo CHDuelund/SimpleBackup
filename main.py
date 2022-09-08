ec62583282477719c3872ae448580325e23c140e
CHDuelund <chr.duelund@outlook.dk>
2022-09-08 22:31:26 +0200

    pick modul fjernet, almindelig menu kodet

--- main.py
+++ main.py
@@ -3,32 +3,42 @@
 import shutil  # modul som gør det muligt at flytte, kopiere og slette filer.
 import os  # modul som giver mulighed for administration af mapper, fx. skift af CWD samt oprettelse/sletning af mapper.
 import sys  # modul som bla. giver mulighed for udlæsning og evt. logning af in- og output direkte fra interpreteren.
-from pick import pick
 from termcolor import colored
-import colorama
-colorama.init()
 from datetime import date  # funktionen date importeres fra datetime modulet, til angivelse af dato ved backupkørsler.
 
 
 # DEFINERING AF FUNKTIONER
 
-# def Full_Backup():
-#    do something
+def Main_Menu():
+    print(colored("Simple Backup by CHDuelund v1.0a \n", "blue", attrs=["bold", "underline"]))
 
-# def Differential_Backup():
-#    do something
+    print(colored("Vælg funktion:", attrs=["underline"]))
+    print("1 = Full Backup \n2 = Differential Backup \n3 = Incremental Backup \n")
 
-# def Incremental_Backup():
-#    do something
+    while (True):
+        function = input("Indtast dit valg: ")
+        if function == "1":
+            Full_Backup()
+            break
+        elif function == "2":
+            Differential_Backup()
+            break
+        elif function == "3":
+            Incremental_Backup()
+            break
+        else:
+            print(colored("Ugyldigt input - prøv igen... \n", "red"))
+
+def Full_Backup():
+    print("Kører Full Backup")
+
+def Differential_Backup():
+    print("Kører Differential Backup")
+
+def Incremental_Backup():
+    print("Kører Incremental Backup")
 
 
 # HOVEDMENU
 
-colored_title = colored("Simple Backup by CHDuelund v1.0a", "blue", attrs=["bold", "underline"])
-title = colored_title + "Vælg venligst backup type: "
-print(colored_title)
-
-wait = input()
-# options = ["Full Backup", "Differential Backup", "Incremental Backup"]
-# option, index = pick(options, title, indicator="=>", default_index=0)
-# print(f"You choosed {option} at index {index}")
+Main_Menu()
