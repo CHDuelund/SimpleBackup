1c5a3af8f753fb840699d6893e1b60de516dd360
CHDuelund <chr.duelund@outlook.dk>
2022-09-12 23:14:40 +0200

    Differential Backup = Work in Progress.

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ from termcolor import colored # modul som gør det muligt at formatere tekst med
 import re # modul som gør det muligt at splitte en string ved brug af regex.
 from datetime import date,datetime  # funktionen date importeres fra datetime modulet, til angivelse af dato ved backupkørsler.
 import csv
-from filecmp import dircmp
+import filecmp
 
 
 # GLOBALE VARIABLER
 
 get_today = date.today() # hent dato til variabel.
+date_today = get_today.strftime("%d-%m-%Y")
 date_stamp = get_today.strftime("_%d_%m_%Y") # konverter dato til ønsket format.
 
 
@@ -42,10 +43,6 @@ def Main_Menu():
             print(colored("Invalid input - please try again...\n", "red"))
 
 
-def Clear_Console():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-
 def Full_Backup():
     print(colored("\nFull Backup\n", "blue", attrs=["bold", "underline"]))
 
@@ -67,10 +64,13 @@ def Full_Backup():
         if folder_exists == True:
             get_time = datetime.now()  # hent tid til variabel.
             time_stamp = get_time.strftime("_%H_%M_%S")  # konverter tid til ønsket format.
+            time_now = get_time.strftime("%H:%M:%S")
 
-            shutil.copytree(source_path, destination_path + folder_name[-1] + "_full" + date_stamp + time_stamp)
+            new_folder_path = destination_path + folder_name[-1] + "_full" + date_stamp + time_stamp
 
-            log_data = [source_path, destination_path, folder_name[-1] + "_full" + date_stamp + time_stamp]
+            shutil.copytree(source_path, new_folder_path)
+
+            log_data = [source_path, destination_path, folder_name[-1] + "_full" + date_stamp + time_stamp, "full", date_today, time_now]
             with open('backup_log.csv', 'a', encoding='UTF8') as f:
                 writer = csv.writer(f)
                 writer.writerow(log_data)
@@ -119,18 +119,41 @@ def Differential_Backup():
             get_time = datetime.now()  # hent tid til variabel.
             time_stamp = get_time.strftime("_%H_%M_%S")  # konverter tid til ønsket format.
 
-            compare = dircmp(source_path, destination_path)
+            new_folder_path = destination_path + folder_name[-1] + "_full" + date_stamp + time_stamp
 
-            def Compare_Files(dcmp):
-                for name in dcmp.diff_files:
-                    print("diff_file %s found in %s and %s" % (name, dcmp.left, dcmp.right))
-                for sub_dcmp in dcmp.subdirs.values():
-                    Compare_Files(sub_dcmp)
+            get_time.strftime("_%H_%M_%S")
 
-            Compare_Files(compare)
+            get_latest_full = Latest_Full() # find seneste full backup kørsel via funktionen Latest_Backup.
+
+            def folder_diff():
+                folder1 = os.listdir(get_latest_full)  # folder containing your files
+                folder2 = os.listdir(source_path)  # the other folder
+
+                for item1 in folder1:
+                    for item2 in folder2:
+                        if (item1 == item2):
+                            res = filecmp.cmp(source_path + item1, get_latest_full + item2)
+
+            data = folder_diff()
+            print(data)
 
 def Incremental_Backup():
     print("Running Incremental Backup")
+
+
+def Clear_Console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def Latest_Full():
+    get_dates = []
+    with open('backup_log.csv') as csv_file:
+        for data in csv.DictReader(csv_file):
+            if data['type'] == "full":
+                get_dates.append(data)
+    latest_full_backup = max(get_dates, key=lambda x: x['date-time'])
+    result = latest_full_backup['bck-path']
+    return result
 
 
 # HOVEDMENU
